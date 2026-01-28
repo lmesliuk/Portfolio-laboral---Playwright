a28e448d338d26ee0a011e84600548ee66b135a2
lmesliuk <luciana_mesliuk@yahoo.com.ar>
2026-01-28 13:30:17 -0300

    First automation case

---
+++ README.md
@@ -0,0 +1,3 @@
+Python Automation Portfolio – Orange HRM
+This project is part of my QA Automation learning process.
+The goal is to build a clean and understandable automation setup step by step.
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hola mundo, soy un portfolio laboral de Playwright")
+
+#print("Hola mundo, soy un portfolio laboral de Playwright")
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.wait_for_timeout(3000)
+    browser.close()
